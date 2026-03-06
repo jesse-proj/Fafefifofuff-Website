389d126625f6fb44ea49d4e54fa9fa7ba382ef31
jesse-proj <jian.santiago39@gmail.com>
2026-03-06 18:47:30 +0800

    Made links work.

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-window.RECENT_BLOGS_NUM = 5;
+window.RECENT_BLOGS_NUM 		= 5;
+window.SERVER_URL 				= 'http:localhost:5000';
 
-window.LATEST_STATUS_URL = 'http:localhost:5000/admin/get-latest-status/Faf';
-window.RECENT_BLOGS_URL = 'http:localhost:5000/blog/get-recent-blogs/' + window.RECENT_BLOGS_NUM;
+window.BLOG_URL 				= window.SERVER_URL + '/blog';
+window.LATEST_STATUS_URL 		= window.SERVER_URL + '/admin/get-latest-status/Faf';
+window.RECENT_BLOGS_URL 		= window.SERVER_URL + '/blog/get-recent-blogs/' + window.RECENT_BLOGS_NUM;
