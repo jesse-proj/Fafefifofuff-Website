d451e741773d4b3e946abb854938f442ed6625c8
jesse-proj <jian.santiago39@gmail.com>
2026-03-28 14:51:00 +0800

    Added custom GitHub contribution graph.

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
-window.RECENT_BLOGS_NUM 		= 5;
-window.SERVER_URL 				= 'http:localhost:5000';
+window.RECENT_BLOGS_NUM = 5;
+window.SERVER_URL = 'http:localhost:5000';
 
-window.BLOG_URL 				= window.SERVER_URL + '/blog';
-window.LATEST_STATUS_URL 		= window.SERVER_URL + '/admin/get-latest-status/Faf';
-window.RECENT_BLOGS_URL 		= window.SERVER_URL + '/blog/get-recent-blogs/' + window.RECENT_BLOGS_NUM;
+window.BLOG_URL = window.SERVER_URL + '/blog';
+window.LATEST_STATUS_URL = window.SERVER_URL + '/admin/get-latest-status/Faf';
+window.RECENT_BLOGS_URL	= window.SERVER_URL + '/blog/get-recent-blogs/' + window.RECENT_BLOGS_NUM;
+
+window.GITHUB_DATA_URL = window.SERVER_URL + '/api/get-github-data';
+
---
+++ scripts/graph.js
@@ -0,0 +1,48 @@
+const graphFallback = document.querySelector('.github-graph-fallback');
+const graphRoot = document.querySelector('.github-graph');
+
+fetch(window.GITHUB_DATA_URL)
+.then(response => {
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return response.json()
+})
+.catch(error => {
+    console.warn('Using fallback API due to fetch failure:', error.message);
+
+    graphFallback.classList.add('active'); // Make this work in the CSS
+
+    return {
+	status: 500 
+    }
+})
+.then(data => {
+    if (data.status == 500) {
+        return undefined;
+    }
+
+    contributionWeeks = data.data.viewer.contributionsCollection.contributionCalendar.weeks;
+    
+    contributionWeeks.forEach(week => {
+    	contributionDays = week.contributionDays;
+
+	contributionDays.forEach(days => {
+	    dayDiv = document.createElement('div');
+
+	    dayDiv.classList.add('contribution');
+	    dayDiv.classList.add(days.contributionLevel);
+	    
+	    graphRoot.appendChild(dayDiv);
+	});
+    });
+})
+
+
+
+
+
+
+
